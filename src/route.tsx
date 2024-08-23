0c24b57b278d6410ad7fd1050c75488b0ea41a79
mahabur-rahman <annur4395@gmail.com>
2024-08-23 19:31:59 +0600

    feat: Error page added for unavaliable route

--- src/route.tsx
+++ src/route.tsx
@@ -1,7 +1,9 @@
+import Error from "./components/Error";
 import Home from "./pages/Home";
 import Preferences from "./pages/Preferences";
 
 export const routes = [
   { path: "/", element: <Home /> },
   { path: "/preferences", element: <Preferences /> },
+  { path: "/*", element: <Error /> },
 ];
